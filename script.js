51d8d06bf5b080738894bf902efcdd000825c290
Oscar Otero <oskar.oc@gmail.com>
2022-11-01 12:33:09 +0100

    Add Player vs Bot function and Switch Marks
    Add some functions to JS file so a Player vs Bot
    can be played. At the moment only easy mode can be
    selected. Also, add a "switch marks" button so marks
    can be tradable. The message with next turn or winner
    was changed to game board upper side.

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const gameBoard = (() => {
     let player2 = "";
     let round = 1;
     let winner = "";
+    let gameMode = "";
 
     const createPlayer = (playerNumber, name) => {
         let symbol = "";
@@ -24,11 +25,19 @@ const gameBoard = (() => {
         }
     };
 
-    const getName = (playerNumber) => {
-        if (playerNumber === "player1") {
-            return player1.name;
+    const switchMarks = () => {
+        let temp = player1.name;
+        player1.name = player2.name;
+        player2.name = temp;
+        resetBoard();
+        if (gameMode === "botEasy") {
+            displayController.displayTurn(player2.name, false);
+            displayController.refreshGameBoard();
+        } else if (gameMode === "botHard") {
+            displayController.displayTurn(player2.name, false);
+            displayController.refreshGameBoard();
         } else {
-            return player2.name;
+            displayController.displayTurn(player1.name, false);
         }
     };
 
@@ -54,28 +63,52 @@ const gameBoard = (() => {
         winner = "";
         round = 1;
         displayController.refreshGameBoard();
-    }
+        if (gameMode === "botEasy" && player1.name === "Bot") {
+            aiBotPlay(aiBotEasy, player1);
+            displayController.refreshGameBoard();
+            displayController.displayTurn(player2.name, false);
+        } else if (gameMode === "botHard" && player1.name === "Bot") {
+            aiBotPlay(aiBotHard, player1);
+            displayController.refreshGameBoard();
+            displayController.displayTurn(player2.name, false);
+        } else if ((gameMode === "botEasy" || gameMode === "botHard") && player1.name !== "Bot") {
+            displayController.displayTurn(player1.name, false);
+        }
+    };
 
     const playRound = (index) => {
         let currentPlayer = "";
+        let bot = "";
         if (round % 2 === 0) {
             currentPlayer = player2;
+            bot = player1;
         } else {
             currentPlayer = player1;
-
+            bot = player2;
         }
         round++;
         setField(index, currentPlayer);
-        console.log(checkWinner(currentPlayer));
-        checkWinner(currentPlayer)
-        if (winner !== "") {
-            console.log("won");
+        checkWinner(currentPlayer);
+        // If playing vs computer
+        if (winner === "" && gameMode === "botEasy") {
+            aiBotPlay(aiBotEasy, bot);
+        } else if (winner === "" && gameMode === "botHard") {
+            aiBotPlay(aiBotHard, bot);
+        }
+        // Display next turn or winner message
+        if (winner === currentPlayer) {
             displayController.displayTurn(currentPlayer.name, true);
+        } else if (winner !== "" && winner !== currentPlayer) {
+            displayController.displayTurn(getNextName(currentPlayer), true);
+        } else if (gameMode === "botEasy") {
+            displayController.displayTurn(currentPlayer.name, false);
         } else {
-            console.log("not yet");
             displayController.displayTurn(getNextName(currentPlayer), false);
         }
-        return currentPlayer;
+    };
+
+    const setGameMode = (mode) => {
+        gameMode = mode;
     };
 
     const isGameOver = () => {
@@ -107,14 +140,24 @@ const gameBoard = (() => {
         });
     };
 
-    const checkVar = () => {
-        console.log(player1, player2);
-        console.log(round);
-        console.log(board);
-        console.log(winner);
+    // AI Bot
+    const aiBotPlay = (difficulty, botPlayer) => {
+        setField(difficulty(), botPlayer);
+        round++;
+        checkWinner(botPlayer);
     };
 
-    return { createPlayer, getName, getNextName, getField, resetBoard, playRound, isGameOver, checkWinner, checkVar };
+    const aiBotEasy = () => {
+        let found = false;
+        let randomField = "";
+        while (found === false) {
+            randomField = Math.floor(Math.random() * 9);
+            if (board[randomField] === "") found = true;
+        }
+        return randomField;
+    };
+
+    return { createPlayer, switchMarks, getField, resetBoard, playRound, setGameMode, isGameOver };
 })();
 
 const displayController = (() => {
@@ -125,14 +168,18 @@ const displayController = (() => {
     // AI or player buttons
     const aiBotBtn = document.getElementById("aiBot");
     const pvpBtn = document.getElementById("playerVsPlayer");
-    const modalDiv = document.querySelector(".modal");
+    const playerModalDiv = document.getElementById("playersModal");
     const overlayDiv = document.querySelector(".overlay");
     const gameDiv = document.querySelector(".game");
     const formElement = document.querySelector(".playersForm");
+    const botModalDiv = document.getElementById("botModal");
+    const botEasyBtn = document.getElementById("botEasyBtn");
+    const botHardBtn = document.getElementById("botHardBtn");
     // Game related items
     const fieldElements = document.querySelectorAll(".field");
     const playerMsg = document.querySelector(".playerMsg");
     const gameRestartBtn = document.getElementById("gameBoardRestart");
+    const switchMarksBtn = document.getElementById("switchMarks");
 
     // Game start
     startGameBtn.addEventListener("click", () => {
@@ -141,19 +188,38 @@ const displayController = (() => {
     });
 
     // Select opponent
-    pvpBtn.addEventListener("click", () => toggleModals());
+    aiBotBtn.addEventListener("click", () => toggleModals(botModalDiv));
+
+    botEasyBtn.addEventListener("click", () => {
+        botGame();
+        gameBoard.setGameMode("botEasy");
+        toggleModals(botModalDiv);
+    });
+
+    botHardBtn.addEventListener("click", () => {
+        botGame();
+        gameBoard.setGameMode("botHard");
+        toggleModals(botModalDiv);
+    });
+
+    pvpBtn.addEventListener("click", () => toggleModals(playerModalDiv));
 
     formElement.addEventListener("submit", e => {
         e.preventDefault();
         gameBoard.resetBoard();
+        gameBoard.setGameMode("pvp");
         gameBoard.createPlayer("player1", document.getElementById("player1Name").value);
         gameBoard.createPlayer("player2", document.getElementById("player2Name").value);
-        toggleModals();
+        toggleModals(playerModalDiv);
         gameDiv.classList.add("active");
         playerMsg.textContent = `It's ${document.getElementById("player1Name").value}'s turn!`
     });
 
-    overlayDiv.addEventListener("click", () => toggleModals());
+    overlayDiv.addEventListener("click", () => {
+        playerModalDiv.classList.remove("active");
+        botModalDiv.classList.remove("active");
+        overlayDiv.classList.remove("active");
+    });
 
     // Player selection
     fieldElements.forEach(element => {
@@ -168,12 +234,14 @@ const displayController = (() => {
     // Restart button
     gameRestartBtn.addEventListener("click", () => {
         gameBoard.resetBoard();
-        playerMsg.textContent = `It's ${gameBoard.getName("player1")}'s turn!`
     });
 
+    // Switch Marks button
+    switchMarksBtn.addEventListener("click", () => gameBoard.switchMarks());
+
     // Functions
-    const toggleModals = () => {
-        modalDiv.classList.toggle("active");
+    const toggleModals = (modal) => {
+        modal.classList.toggle("active");
         overlayDiv.classList.toggle("active");
     };
 
@@ -187,14 +255,22 @@ const displayController = (() => {
         if (fieldElements[field].textContent !== "") return true;
     };
 
+    const botGame = () => {
+        gameBoard.resetBoard();
+        gameBoard.createPlayer("player1", "Player");
+        gameBoard.createPlayer("player2", "Bot");
+        gameDiv.classList.add("active");
+        playerMsg.textContent = `It's Player's turn!`
+    };
+
     // Display winner
-    const displayTurn = (player, win) => {
+    const displayTurn = (playerName, win) => {
         if (win) {
-            playerMsg.textContent = `${player} won!`;
+            playerMsg.textContent = `${playerName} won!`;
         } else if (!win && gameBoard.isGameOver()) {
             playerMsg.textContent = `It's a tie!`;
         } else {
-            playerMsg.textContent = `It's ${player}'s turn!`;
+            playerMsg.textContent = `It's ${playerName}'s turn!`;
         }
     };
 
